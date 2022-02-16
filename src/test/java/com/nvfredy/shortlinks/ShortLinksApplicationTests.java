5d947663f72914b225633a3d36a320abe7d74d8f
Andrey Fyodorov <afedorov@rdlab.info>
2022-02-16 08:21:05 +0500

    Initial commit

---
+++ src/test/java/com/nvfredy/shortlinks/ShortLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvfredy.shortlinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
